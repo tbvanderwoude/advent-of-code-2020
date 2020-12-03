2d0074d6873727c1ea52c5f146771ad407e9990a
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-03 09:31:23 +0100

    Made solution more functional

--- src/Day03.rs
+++ src/Day03.rs
@@ -2,14 +2,8 @@ use std::fs;
 fn main() {
     let text = fs::read_to_string(&"inputs/input03.txt".to_string()).expect("Could not open input");
     let map = text.split("\n").collect::<Vec<&str>>();
-    let directions = [(1,1),(3,1),(5,1),(7,1),(1,2)];
-    let mut counts = vec![];
-    for dir in directions.iter(){
-        let final_x: usize = map[0].len();
-        let final_y: usize = map.len().clone()-1;
-        let tree_count: i64 = (0..final_y).filter(|y| y % dir.1==0).map(|y|( map[y].as_bytes()[(dir.0*y/dir.1) % final_x]=='#' as u8) as i64).fold(0,|acc,x| acc+x);
-        counts.push(tree_count);
-        println!("{:?}: {} trees", dir,tree_count);
-    }
-    println!("Product: {:?}", counts.iter().fold(1 ,|acc,x| acc*x));
+    let directions: Vec<(usize,usize)> = vec![(1,1),(3,1),(5,1),(7,1),(1,2)];
+    let counts = directions.iter().map(|dir| (0..(map.len().clone()-1)).filter(|y| y % dir.1==0).
+    map(|y|( map[y].as_bytes()[(dir.0*y/dir.1) % map[0].len()]=='#' as u8) as i64).fold(0,|acc,x| acc+x));
+    println!("Product: {:?}", counts.fold(1 ,|acc,x| acc*x));
 }
