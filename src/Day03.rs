228ac9953a03ecd602122d9d2020058750c7bfe4
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-03 14:20:31 +0100

    Minimized rust solution

--- src/Day03.rs
+++ src/Day03.rs
@@ -1,10 +1,7 @@
 use std::fs;
 fn main() {
-    let text = fs::read_to_string(&"inputs/input03.txt".to_string()).expect("Could not open input");
-    let map = text.split("\n").collect::<Vec<&str>>();
-    let directions: Vec<(usize,usize)> = vec![(1,1),(3,1),(5,1),(7,1),(1,2)];
-    let counts = directions.iter().map(|dir| (0..(map.len().clone()-1)).filter(|y| y % dir.1==0).
-    map(|y|( map[y].as_bytes()[(dir.0*y/dir.1) % map[0].len()]=='#' as u8) as i64).fold(0,|acc,x| acc+x));
-    println!("{:?}", counts.collect::<Vec<i64>>());
-    // println!("Product: {:?}", counts.fold(1 ,|acc,x| acc*x));
+    let map = fs::read_to_string(&"inputs/input03.txt".to_string()).expect("Could not open input");
+    let counts = [(1,1),(3,1),(5,1),(7,1),(1,2)].iter().map(|dir| map.lines().step_by(dir.1).zip((0..).step_by(dir.0)).
+    filter(|&(line,x)| line.as_bytes()[x % line.len()]=='#' as u8).count());
+    println!("Product: {:?}", counts.product::<usize>());
 }
