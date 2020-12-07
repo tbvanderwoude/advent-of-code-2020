13eb1e4d1a7e021c7be1c62d791fdbb299605dae
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-07 11:38:35 +0100

    Finally finished day 7

--- src/Day07.py
+++ src/Day07.py
@@ -1,38 +1,49 @@
 import re
 
 ps = open("inputs/input07.txt", "r").read().splitlines()
-r = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
-e1 = re.compile(r'(\w+ \w+) bags contain ((\d+) (\w+ \w+) bags*, )*((\d+) (\w+ \w+) bags*.)')
-e2 = re.compile(r'(\w+ \w+) bags contain no other bags.')
+e1 = re.compile(r'^(\w+ \w+)')
+e2 = re.compile(r'(\d+) (\w+ \w+)')
 rules = {}
+# rules = {(b := ln.replace(" bags", "").replace(" bag", "").split(" contain "))[0]: {c[2::]: int(c[0]) for c in b[1].strip().replace(".", "").split(", ") if c != "no other"} for ln in open("inputs/input07.txt").readlines()}
+
 for s in ps:
-    matches = e1.match(s)
-    if matches:
-        grps = list(filter(lambda x: x, matches.groups()))
-        # print(grps)
-        if grps[0] in rules:
-            print("This is already in there!")
-        rules[grps[0]] = list(
-            zip([int(grps[i]) for i in range(2, len(grps), 3)], [grps[i] for i in range(3, len(grps), 3)]))
-        rules[grps[0]].append((1,grps[0]))
-    matches2 = e2.match(s)
-    if matches2:
-        grps = matches2.groups()
-        rules[grps[0]] = []
+    if s:
+        rules[e1.match(s)[0]] = list(map(lambda x: (int(x[0]),x[1]), e2.findall(s)))
+
 print(rules)
 
-colors = ['shiny gold']
+# colors = ["shiny gold"]
 
-new_color = True
-while new_color:
-    new_color = False
-    for (color, rule) in rules.items():
-        for (prod_count, prod_color) in rule:
-            if prod_color in colors and color not in colors:
-                print("%s also works as it produces %s" % (color, prod_color))
-                colors.append(color)
-                new_color = True
-                # print(colors)
+# prev = {"shiny gold" : 1}
+# bags = set()
+# while len(prev) > 0:
+#     prev = set(b for b in rules for p in prev if p in rules[b])
+#     bags |= prev
+# print(len(bags))
+# new_color = True
+#
+# while new_color:
+#     new_color = False
+#     for (color, rule) in rules.items():
+#         for (num,prod_color) in rule:
+#             if prod_color in colors and color not in colors:
+#                 # print("%s also works as it produces %s" % (color, prod_color))
+#                 colors.append(color)
+#                 new_color = True
+#                 # print(colors)
+def bags(root):
+    if not rules[root]:
+        return 1
+    return 1+sum([num*(bags(col)) for (num,col) in rules[root]])
 
-print("Out of %s bag colors (excluding shiny gold), %s should contain gold bags" % (len(rules), len(colors) - 1))
-# print(e1.match(ps[2]).groups())
+def contains(root, leaf):
+    if root == leaf:
+        return True
+    contain = False
+    for (num,prod_color) in rules[root]:
+        if prod_color!=root and contains(prod_color, leaf):
+            contain = True
+    return contain
+
+print(sum([contains(key,"shiny gold") for key in rules])-1)
+print(bags("shiny gold")-1)
