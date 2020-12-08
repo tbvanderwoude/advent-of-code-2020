911e4d51c99165bc3f28558f997837c795e44b8f
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-08 08:29:26 +0100

    Finished part 2

--- src/Day08.py
+++ src/Day08.py
@@ -1,5 +1,23 @@
 import re
 
+def sim(instr):
+    progcount = 0
+    progcounts = set()
+    acc = 0
+    while progcount not in progcounts:
+        if progcount>=len(instr):
+            return (True,acc)
+        progcounts.add(progcount)
+        (opcode, arg) = instr[progcount]
+        if opcode == "jmp":
+            progcount += arg
+        elif opcode == "nop":
+            progcount += 1
+        elif opcode == "acc":
+            acc += arg
+            progcount += 1
+    return (False,acc)
+
 ps = open("inputs/input08.txt", "r").read().strip().splitlines()
 e = re.compile(r'(nop|acc|jmp) ([+-]\d+)')
 rules = {}
@@ -13,17 +31,21 @@ for s in ps:
     instr.append((opcode,arg))
     print("%s: %i"%(opcode,arg))
 
-progcount = 0
-progcounts = set()
-acc = 0
-while progcount not in progcounts:
-    progcounts.add(progcount)
-    (opcode, arg) = instr[progcount]
-    if opcode=="jmp":
-        progcount+=arg
-    elif opcode=="nop":
-        progcount+=1
-    elif opcode=="acc":
-        acc+=arg
-        progcount+=1
-print(acc)
+jmp_nop_indices = filter(lambda x: x[1][0]=="nop" or x[1][0]=="jmp",enumerate(instr))
+
+for (i, (op,arg)) in jmp_nop_indices:
+    # subs
+    if op=="jmp":
+        instr[i] = ("nop",arg)
+        ret = sim(instr)
+        if ret[0]:
+            print(ret[1])
+        instr[i]= ("jmp",arg)
+    if op == "nop":
+        instr[i] = ("jmp",arg)
+        ret = sim(instr)
+        if ret[0]:
+            print(ret[1])
+        instr[i] = ("nop",arg)
+
+print(sim(instr))
