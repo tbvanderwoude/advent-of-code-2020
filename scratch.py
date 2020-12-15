a80bfc12a5a31cca1ec719316cd0810ac3ced112
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-15 09:58:00 +0100

    Part 2

---
+++ scratch.py
@@ -0,0 +1,25 @@
+ls = open("inputs/input15.txt", "r").read().splitlines()
+ns = list(map(lambda s: int(s),ls[0].split(",")))
+print(ns)
+def last_index(ns,e):
+    return (len(ns)-1)-list(reversed(ns)).index(e)
+
+spoken = dict(map(lambda x: (x[1],x[0]),enumerate(ns[:-1])))
+
+last = ns[-1]
+n = 2019
+for i in range(2,n):
+    # print(last,spoken)
+    if last in spoken:
+        ix = spoken[last]
+        # print("%i was spoken at %i"%(last,ix))
+        spoken[last] = i
+        last = i-ix
+    else:
+        # print("%i was not spoken yet"%(last))
+        spoken[last] = i
+        last = 0
+    # print("%i is spoken\n"%(last))
+
+print("The last spoken number is")
+print(last)
--- src/Day15.py
+++ src/Day15.py
@@ -1,18 +1,35 @@
-ls = open("inputs/input15.txt", "r").read().splitlines()
-ns = list(map(lambda s: int(s),ls[0].split(",")))
-print(ns)
 def last_index(ns,e):
     return (len(ns)-1)-list(reversed(ns)).index(e)
-while len(ns)<30000000:
-    front = ns[:len(ns)-1]
-    last = ns[len(ns)-1]
-    # print(front,last)
-    if last in front:
-        ix = last_index(front,last);
-        # print(ix)
-        ns.append((len(ns)-1)-ix)
-    else:
-        ns.append(0)
-print("The final list of numbers is")
-print(ns)
-print(ns[-1])
+
+def solve_slow(ns):
+    while len(ns)<2020:
+        front = ns[:len(ns)-1]
+        last = ns[len(ns)-1]
+        if last in front:
+            ix = last_index(front,last)
+            ns.append((len(ns)-1)-ix)
+        else:
+            ns.append(0)
+    print("The final list of numbers is")
+    print(ns)
+    print(ns[-1])
+
+# perhaps I should start with this type of solution anyway?
+def solve_faster(ns):
+    spoken = dict(map(lambda x: (x[1], x[0]), enumerate(ns[:-1])))
+    last = ns[-1]
+    n = 30000000
+    for i in range(len(ns)-1, n - 1):
+        if last in spoken:
+            ix = spoken[last]
+            spoken[last] = i
+            last = i - ix
+        else:
+            spoken[last] = i
+            last = 0
+    print("The last spoken number is")
+    print(last)
+
+ls = open("inputs/input15.txt", "r").read().splitlines()
+solve_slow(list(map(lambda s: int(s),ls[0].split(","))))
+solve_faster(list(map(lambda s: int(s),ls[0].split(","))))
