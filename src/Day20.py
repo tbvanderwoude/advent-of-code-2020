ef0c39f1a008395415e9578fde424b53ef1f9716
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-20 22:53:40 +0100

    Refactored some things

--- src/Day20.py
+++ src/Day20.py
@@ -25,48 +25,41 @@ class Tile:
         new_img = deepcopy(self.img)  
         for i in range(len(new_img)):
             new_img[i] = [self.img[len(new_img)-1-j][i] for j in range(len(self.img))]  
-        print(self.img)
-        print(new_img)
-        self.img = new_img
+        return Tile(self.tid,new_img)
     
     def flip(self):
         new_img = deepcopy(self.img)
         for i in range(len(new_img)):
             new_img[i] = list(reversed(new_img[i]))
-        self.img = new_img
+        return Tile(self.tid,new_img)
 
 def matches(match_tile,tiles):
     count = 0
     matches = []
-    for (i,match_side) in enumerate(match_tile.sides()): 
+    for (i,match_side) in enumerate(tiles[match_tile].sides()): 
         #print(match_side)
         for tile in [t for t in tiles if t!=match_tile]:
-            for side in tile.sides(): 
+            for side in tiles[tile].sides(): 
                 rev = list(reversed(side))
                 if match_side == side or match_side ==rev:
                     count+=1
-                    matches.append((i,tile.tid))
-    print("%i has %i matches"%(match_tile.tid,count))
+                    matches.append((i,tile))
+    print("%i has %i matches"%(match_tile,count))
     print(matches)
-    return count, match_tile.tid
+    return count
         
 parts = list(open("inputs/input20.txt", "r").read().replace(".","0").replace("#","1").split("\n\n"))
 print(parts)
-tiles = []
+tiles = {}
 for part in parts:
     lines = part.splitlines()
     tid = int(lines[0][4:9])
     image = list(map(lambda l: list(l),lines[1:]))
     print(tid)
-    tiles.append(Tile(tid,image))
-target_tile = tiles[0]
+    tiles[tid] = Tile(tid,image)
 prod = 1
-for target_tile in tiles:
-    c,tid = matches(target_tile,tiles)
+for tid in tiles:
+    c = matches(tid,tiles)
     if c==2:
         prod*=tid
 print(prod)
-test_tile = Tile(42,[[1,2,3],[4,5,6],[7,8,9]])
-test_tile.rotate_quarter()
-test_tile = Tile(42,[[1,2,3],[4,5,6],[7,8,9]])
-test_tile.flip()
