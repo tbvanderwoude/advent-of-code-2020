f1caec43e9406da38943948126668a6d482f3937
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-20 22:09:11 +0100

    Implemented transformations

--- src/Day20.py
+++ src/Day20.py
@@ -1,13 +1,55 @@
 from copy import copy, deepcopy
+from enum import Enum
+
+class Side(Enum):
+    TOP = 0
+    RIGHT = 1
+    BOTTOM = 2
+    LEFT = 3
 
 class Tile:
-    def __init__(self,tid,top,right,bottom,left):
+    def __init__(self,tid,img):
         self.tid = tid
-        self.top = top
-        self.right = right
-        self.bottom = bottom
-        self.left = left
+        self.img = img
+    def sides(self):
+        top = copy(self.img[0])
+        bottom = copy(self.img[-1])
+        left = []
+        right = []
+        for i in range(len(self.img[0])):
+            right.append(self.img[i][-1])
+            left.append(self.img[i][0])
 
+        return [top,right,list(reversed(bottom)),list(reversed(left))]
+    def rotate_quarter(self):
+        new_img = deepcopy(self.img)  
+        for i in range(len(new_img)):
+            new_img[i] = [self.img[len(new_img)-1-j][i] for j in range(len(self.img))]  
+        print(self.img)
+        print(new_img)
+        self.img = new_img
+    
+    def flip(self):
+        new_img = deepcopy(self.img)
+        for i in range(len(new_img)):
+            new_img[i] = list(reversed(new_img[i]))
+        self.img = new_img
+
+def matches(match_tile,tiles):
+    count = 0
+    matches = []
+    for (i,match_side) in enumerate(match_tile.sides()): 
+        #print(match_side)
+        for tile in [t for t in tiles if t!=match_tile]:
+            for side in tile.sides(): 
+                rev = list(reversed(side))
+                if match_side == side or match_side ==rev:
+                    count+=1
+                    matches.append((i,tile.tid))
+    print("%i has %i matches"%(match_tile.tid,count))
+    print(matches)
+    return count, match_tile.tid
+        
 parts = list(open("inputs/input20.txt", "r").read().replace(".","0").replace("#","1").split("\n\n"))
 print(parts)
 tiles = []
@@ -15,26 +57,16 @@ for part in parts:
     lines = part.splitlines()
     tid = int(lines[0][4:9])
     image = list(map(lambda l: list(l),lines[1:]))
-    border = image[0]
-    top = copy(image[0])
-    bottom = copy(image[-1])
-    left = []
-    right = []
-    for i in range(len(image[0])):
-        right.append(image[i][-1])
-        left.append(image[i][0])
     print(tid)
-    print("Top: {}\nRight: {}\nBottom: {}\nLeft: {}".format(top,right,bottom,left))
-    tiles.append(Tile(tid,top,right,bottom,left))
+    tiles.append(Tile(tid,image))
 target_tile = tiles[0]
-target_side = target_tile.right
-for t in tiles[1:]:
-    for side in [t.top,t.right,t.bottom,t.left]:
-        print("Side: {}".format(side))
-        mirr = list(reversed(side[len(side)//2:]))
-        print(mirr)
-        mirr.extend(reversed(side[:len(side)//2]))
-        print(mirr)
-        print(side)
-        if target_side == side or target_side ==reversed(side) or target_side == mirr:
-            print("A match! A match!")
+prod = 1
+for target_tile in tiles:
+    c,tid = matches(target_tile,tiles)
+    if c==2:
+        prod*=tid
+print(prod)
+test_tile = Tile(42,[[1,2,3],[4,5,6],[7,8,9]])
+test_tile.rotate_quarter()
+test_tile = Tile(42,[[1,2,3],[4,5,6],[7,8,9]])
+test_tile.flip()
