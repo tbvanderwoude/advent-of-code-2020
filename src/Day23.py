f3862c1562b2679342fb88eea6cb424e7806c3c3
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-23 13:26:13 +0100

    Implemented linked list-like ds

--- src/Day23.py
+++ src/Day23.py
@@ -37,14 +37,64 @@ def part1(input_str):
     after_1.remove(1)
     return "".join(map(str,after_1))
 
+def to_str(nxt,start):
+    s = ""
+    it = start
+    for i in range(len(nxt)-1):
+       s+= str(it)
+       it = nxt[it]
+    return s
+
+    
 def part2(input_str):
-    pass
+    labels = list(map(int,list(input_str)))
+    print(labels)
+    n = 9 
+    m = 10
+    nxt = [0] * (n+1)
+    for i in range(n+1):
+        if i==0:
+            nxt[0]=-1
+        elif i>=1 and i<=8:
+            nxt[labels[i-1]] = labels[i]
+        elif i==9:
+            nxt[labels[8]] = labels[0]
+        elif i==n:
+            nxt[i-1] = labels[0] 
+        else:
+            nxt[i] = i+1
+
+    print(list(enumerate(nxt)))
+    current = labels[0]
+    for i in range(m):
+        print(current)
+        print(to_str(nxt,current))
+        p1 = nxt[current]
+        p2 = nxt[p1]
+        p3 = nxt[p2]
+        print(p1,p2,p3)
+        nxt[current] = nxt[p3]
+        p = [p1,p2,p3]
+        dest  = current
+        while dest in p or dest==current:
+            if dest == 1:
+                print("Wrap around!")
+                dest = n
+            else:
+                dest-=1
+        nxt_new = nxt[dest]
+        nxt[p3] = nxt_new
+        nxt[dest] = p1
+        current = nxt[current]
+    print(to_str(nxt,1))
+
+    n1 = nxt[1]
+    n2 = nxt[n1]
+    print(n1,n2)
+    return n1*n2
+
 
 # imagine 10 11 12 ... 10000000 at the end
 blocks = "247819356"
 print(part1(blocks))
-print(part2(blocks))
-
-
-
-
+print(part2("389125467"))
