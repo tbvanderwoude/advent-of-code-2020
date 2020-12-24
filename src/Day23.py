b14fbd054c99ecd28a125531d7e324deb8d9de3c
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-24 10:35:00 +0100

    Reformatted 22,23,24

--- src/Day23.py
+++ src/Day23.py
@@ -1,89 +1,92 @@
 from functools import reduce
-from copy import deepcopy,copy
+from copy import deepcopy, copy
 from collections import deque
 
-def rot(ls,n):
+
+def rot(ls, n):
     new_ls = []
     for i in range(len(ls)):
-        new_ls.append(ls[(i+n)%len(ls)])
+        new_ls.append(ls[(i + n) % len(ls)])
     return new_ls
 
+
 def part1(input_str):
-    labels = deque(map(int,list(input_str)))
+    labels = deque(map(int, list(input_str)))
     moves = 100
     c = labels[0]
     picked = []
     for i in range(moves):
-        labels.rotate(-1) 
+        labels.rotate(-1)
         for j in range(3):
             picked.append(labels.popleft())
         labels.rotate(1)
         dest = -1
-        for sub in range(1,10):
-            cand = (c-sub)%10
+        for sub in range(1, 10):
+            cand = (c - sub) % 10
             if cand in labels:
-                dest = cand 
+                dest = cand
                 break
-        c = labels[1] 
+        c = labels[1]
         labels.rotate(-1)
         dest_index = labels.index(dest)
-        labels.rotate(-dest_index-1)
+        labels.rotate(-dest_index - 1)
         labels.extendleft(reversed(picked))
-        labels.rotate(dest_index+1)
+        labels.rotate(dest_index + 1)
         picked = []
     onedex = labels.index(1)
-    after_1 = rot(labels,onedex)
+    after_1 = rot(labels, onedex)
     after_1.remove(1)
-    return "".join(map(str,after_1))
+    return "".join(map(str, after_1))
 
-def to_str(nxt,start):
+
+def to_str(nxt, start):
     s = ""
     it = start
-    for i in range(len(nxt)-1):
-       s+= str(it)
-       it = nxt[it]
+    for i in range(len(nxt) - 1):
+        s += str(it)
+        it = nxt[it]
     return s
 
-    
+
 def part2(input_str):
-    labels = list(map(int,list(input_str)))
-    n = 1000000 
+    labels = list(map(int, list(input_str)))
+    n = 1000000
     m = 10000000
-    nxt = [0] * (n+1)
-    for i in range(n+1):
-        if i==0:
-            nxt[0]=-1
-        elif i>=1 and i<=8:
-            nxt[labels[i-1]] = labels[i]
-        elif i==9:
-            nxt[labels[8]] = i+1 
-        elif i==n:
-            nxt[i] = labels[0] 
+    nxt = [0] * (n + 1)
+    for i in range(n + 1):
+        if i == 0:
+            nxt[0] = -1
+        elif i >= 1 and i <= 8:
+            nxt[labels[i - 1]] = labels[i]
+        elif i == 9:
+            nxt[labels[8]] = i + 1
+        elif i == n:
+            nxt[i] = labels[0]
         else:
-            nxt[i] = i+1
+            nxt[i] = i + 1
     current = labels[0]
     for i in range(m):
         p1 = nxt[current]
         p2 = nxt[p1]
         p3 = nxt[p2]
         nxt[current] = nxt[p3]
-        p = [p1,p2,p3]
-        dest  = current
-        while dest in p or dest==current:
+        p = [p1, p2, p3]
+        dest = current
+        while dest in p or dest == current:
             if dest == 1:
                 dest = n
             else:
-                dest-=1
+                dest -= 1
         nxt_new = nxt[dest]
         nxt[p3] = nxt_new
         nxt[dest] = p1
         current = nxt[current]
     n1 = nxt[1]
     n2 = nxt[n1]
-    return n1*n2
+    return n1 * n2
 
 
 # imagine 10 11 12 ... 10000000 at the end
 blocks = "247819356"
-print("Part 1: %s"%part1(blocks))
-print("Part 2: %i"%part2(blocks))
+print("Part 1: %s" % part1(blocks))
+print("Part 2: %i" % part2(blocks))
--- src/Day24.py
+++ src/Day24.py
@@ -1,34 +1,43 @@
-import re 
+import re
 from copy import copy
 from functools import reduce
-def get_neighs(c,dir_map):
-    return set(map(lambda x:(x[0]+c[0],x[1]+c[1],x[2]+c[2]),dir_map.values()))
 
-coords = open("inputs/input24.txt","r").read().splitlines()
-dir_map = {"e":(1,-1,0),"se":(0,-1,1),"sw":(-1,0,1),"w":(-1,1,0),"nw":(0,1,-1),"ne":(1,0,-1)}
+
+def get_neighs(c, dir_map):
+    return set(map(lambda x: (x[0] + c[0], x[1] + c[1], x[2] + c[2]), dir_map.values()))
+
+
+coords = open("inputs/input24.txt", "r").read().splitlines()
+dir_map = {
+    "e": (1, -1, 0),
+    "se": (0, -1, 1),
+    "sw": (-1, 0, 1),
+    "w": (-1, 1, 0),
+    "nw": (0, 1, -1),
+    "ne": (1, 0, -1),
+}
 ex = "|".join(list(dir_map.keys()))
 r = re.compile(ex)
 blacks = set()
-ref = (0,0,0)
+ref = (0, 0, 0)
 for coord in coords:
     mtch = r.findall(coord)
-    deltas = list(map(dir_map.get,mtch))
-    hex_coord = reduce(lambda x,y: (x[0]+y[0],x[1]+y[1],x[2]+y[2]),deltas)
+    deltas = list(map(dir_map.get, mtch))
+    hex_coord = reduce(lambda x, y: (x[0] + y[0], x[1] + y[1], x[2] + y[2]), deltas)
     if hex_coord in blacks:
         blacks.remove(hex_coord)
     else:
         blacks.add(hex_coord)
-print("Number of starting black tiles: %i"%len(blacks))
+print("Number of starting black tiles: %i" % len(blacks))
 days = 10
-
+active = set()
 for day in range(days):
     print(len(blacks))
-    active = set()
     for blk in blacks:
-        active = active.union(get_neighs(blk,dir_map))
-    new_blacks = copy(blacks) 
+        active = active.union(get_neighs(blk, dir_map))
+    new_blacks = copy(blacks)
     for act in active:
-        neigh = get_neighs(act,dir_map)
+        neigh = get_neighs(act, dir_map)
         n_blk = len(blacks.intersection(neigh))
         if act in blacks:
             if n_blk == 0 or n_blk > 2:
@@ -37,5 +46,3 @@ for day in range(days):
             if n_blk == 2:
                 new_blacks.add(act)
     blacks = new_blacks
-                
-
