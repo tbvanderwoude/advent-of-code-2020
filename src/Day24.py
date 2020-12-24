dfb16fb9fed2ab2b03ff87e5e8430d708f96f1fc
Thom van der Woude <tbvanderwoude@gmail.com>
2020-12-24 11:23:32 +0100

    Solved part 2

--- src/Day24.py
+++ src/Day24.py
@@ -1,38 +1,35 @@
 import re
-from copy import copy
+from copy import copy, deepcopy
 from functools import reduce
 
 
 def get_neighs(c, dir_map):
-    return set(map(lambda x: (x[0] + c[0], x[1] + c[1], x[2] + c[2]), dir_map.values()))
+    return set(map(lambda x: (x[0] + c[0], x[1] + c[1]), dir_map.values()))
 
 
 coords = open("inputs/input24.txt", "r").read().splitlines()
 dir_map = {
-    "e": (1, -1, 0),
-    "se": (0, -1, 1),
-    "sw": (-1, 0, 1),
-    "w": (-1, 1, 0),
-    "nw": (0, 1, -1),
-    "ne": (1, 0, -1),
+    "e": (1, 0),
+    "se": (0, 1),
+    "sw": (-1, 1),
+    "w": (-1, 0),
+    "nw": (0, -1),
+    "ne": (1, -1),
 }
-ex = "|".join(list(dir_map.keys()))
+ex = "|".join(dir_map.keys())
 r = re.compile(ex)
 blacks = set()
-ref = (0, 0, 0)
 for coord in coords:
-    mtch = r.findall(coord)
-    deltas = list(map(dir_map.get, mtch))
-    hex_coord = reduce(lambda x, y: (x[0] + y[0], x[1] + y[1], x[2] + y[2]), deltas)
+    deltas = list(map(dir_map.get, r.findall(coord)))
+    hex_coord = reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), deltas)
     if hex_coord in blacks:
         blacks.remove(hex_coord)
     else:
         blacks.add(hex_coord)
 print("Number of starting black tiles: %i" % len(blacks))
-days = 10
-active = set()
+days = 100
 for day in range(days):
-    print(len(blacks))
+    active = copy(blacks)
     for blk in blacks:
         active = active.union(get_neighs(blk, dir_map))
     new_blacks = copy(blacks)
@@ -46,3 +43,4 @@ for day in range(days):
             if n_blk == 2:
                 new_blacks.add(act)
     blacks = new_blacks
+print("Number of black tiles facing up after %i days: %i" % (days, len(blacks)))
